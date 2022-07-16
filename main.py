7205b64fc6943c9826150c6d20c6cadf1dee6755
Trojan_tazz <tshiamomosiane008@gmail.com>
2022-07-16 12:10:54 +0800

    postgres

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ gravatar = Gravatar(app, size=100, rating='g', default='retro', force_default=Fa
                     base_url=None)
 
 # CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("postgresql://weozxzlbmqqhgg:e2db05e99ffdbd21ac9b539b6718480eb4325aa3f23a81e0f0b8ad0e4507c584@ec2-3-217-14-181.compute-1.amazonaws.com:5432/d7bdmj2tkpd7ft", "sqlite:///blog.db")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL1", "sqlite:///blog.db")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 login_manager = LoginManager()
